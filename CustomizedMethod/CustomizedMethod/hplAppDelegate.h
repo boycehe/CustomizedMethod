17d7a3d61dea7d7cb83e0563e55df3a9fb2c1ac7
boycehe <hepeilin1987@qq.com>
2013-12-20 13:24:07 +0800

    初始化工程
    init project

---
+++ CustomizedMethod/CustomizedMethod/hplAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  hplAppDelegate.h
+//  CustomizedMethod
+//
+//  Created by hepeilin on 13-12-20.
+//  Copyright (c) 2013年 minfo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface hplAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CustomizedMethod/CustomizedMethod/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CustomizedMethod
+//
+//  Created by hepeilin on 13-12-20.
+//  Copyright (c) 2013年 minfo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "hplAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([hplAppDelegate class]));
+    }
+}
